e0b2be1dbd265ebef280526f92c4b99868b5f636
Tama <arifnovrian@gmail.com>
2023-06-07 00:10:49 +0700

    Feat Logout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    methods: {
+        logout() {
+            localStorage.clear()
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="navbar bg-base-300">
+        <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl">MyQuran</a>
+        </div>
+        <div class="flex-none">
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                        </svg>
+                    </div>
+                </label>
+                <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                    <div class="card-body">
+                        <div class="card-actions">
+                            <button class="btn btn-primary btn-block">View Mark</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img
+                            src="https://e7.pngegg.com/pngimages/416/62/png-clipart-anonymous-person-login-google-account-computer-icons-user-activity-miscellaneous-computer.png" />
+                    </div>
+                </label>
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                        <button @click.prevent="logout()" class="btn btn-primary btn-block">Logout</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
